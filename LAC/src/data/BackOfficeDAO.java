40a26fa58efae39aed5e00d36fb1472f5f00f78d
admin <admin@pcenterjava-10>
2018-08-31 09:05:31 +0200

    r

--- LAC/src/data/BackOfficeDAO.java
+++ LAC/src/data/BackOfficeDAO.java
@@ -11,79 +11,103 @@ import model.Usuario;
 import utilities.Pantalla;
 
 public class BackOfficeDAO implements IBackOfficeDAO {
-	
-    // Not thread-safe
-    public Connection con = null;
 
-    // package level access
-    public BackOfficeDAO() {
-    	con = new ConexionDB().getConnection();
-    }
+	// Not thread-safe
+	public Connection con = null;
 
-    //añade un nuevo usuario a la bbdd
+	// package level access
+	public BackOfficeDAO() {
+		con = new ConexionDB().getConnection();
+	}
+
+	// añade un nuevo usuario a la bbdd
 	public void altaUsuario(Usuario u) {
-        Statement st = null;
-		
-        try {
-            ConexionDB con = new ConexionDB();
-            st = con.getConnection().createStatement();
-            String q = "INSERT INTO `lac`.`usuarios` (`dni`, `nombre`, `apellidos`, `rol`, `contrasena`, `direccion`)"
-            			+"VALUES (`"+u.getDni()+"`, `"+ u.getNombre() +"`, `"+ u.getApellidos() 
-            			+"`, `"+ u.getRol() +"`, `"+ u.getContrasena() +"`, `"+ u.getDni() +"`);";
-            Pantalla.write(q);
-            st.executeQuery(q);
-            con.getConnection().close();
-        } catch (SQLException ex) {
-            Logger.getLogger(BackOfficeDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-	}
-	
-	//elimina un usuario de la bbdd segun el dni (Para el 3er Sprint)
-//	public void bajaUsuario(Usuario u) {
-//		Statement st = null;
-//        try {
-//            ConexionDB con = new ConexionDB();
-//            st = con.getConnection().createStatement();
-//            String q = "DELETE FROM `lac`.`usuarios` WHERE dni ='" + u.getDni() + "';";
-//            Pantalla.write(q);
-//            st.executeQuery(q);
-//            con.getConnection().close();
-//        } catch (SQLException ex) {
-//            Logger.getLogger(BackOfficeDAO.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//		
-//	}
+		Statement st = null;
 
-	//añade una nueva camiseta a la bbdd
-	public void altaCamiseta(Camiseta c) {
-        Statement st = null;
-        try {
-            ConexionDB con = new ConexionDB();
-            st = con.getConnection().createStatement();
-            String q = "INSERT INTO `lac`.`camisetas` VALUES ('" + c.getId() + "','" + c.getColor() + "','" + c.getPrecio() + "','"+ c.getGenero() +"','" + c.getTalla() + "','" + c.getCategoria() + "','"+ c.getStock() +"','" + c.getUrl() + "','" + c.getDescripcion()+ "')";
-            Pantalla.write(q);
-            st.executeQuery(q);
-            con.getConnection().close();
-        } catch (SQLException ex) {
-            Logger.getLogger(BackOfficeDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
+		try {
+			ConexionDB con = new ConexionDB();
+			st = con.getConnection().createStatement();
+			String q = "INSERT INTO `lac`.`usuarios` (`dni`, `nombre`, `apellidos`, `rol`, `contrasena`, `direccion`)"
+					+ "VALUES (`" + u.getDni() + "`, `" + u.getNombre() + "`, `" + u.getApellidos() + "`, `"
+					+ u.getRol() + "`, `" + u.getContrasena() + "`, `" + u.getDni() + "`);";
+			Pantalla.write(q);
+			st.executeQuery(q);
+			con.getConnection().close();
+		} catch (SQLException ex) {
+			Logger.getLogger(BackOfficeDAO.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
-	
-	//elimina una camiseta de la bbdd segun el id
-//	para el 3er sprint
-//	public void bajaCamiseta(Camiseta c) {
-//		Statement st = null;
-//        try {
-//            ConexionDB con = new ConexionDB();
-//            st = con.getConnection().createStatement();
-//            String q = "DELETE FROM `lac`.`camisetas` WHERE id ='" + c.getId() + "';";
-//            Pantalla.write(q);
-//            st.executeQuery(q);
-//            con.getConnection().close();
-//        } catch (SQLException ex) {
-//            Logger.getLogger(BackOfficeDAO.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//		
-//	}
+
+	public boolean comprobarUsuarios(String usuario, String contrasena) throws SQLException {
+		Statement st = null;
+		ConexionDB con = new ConexionDB();
+		st = con.getConnection().createStatement();
+
+		String q = "SELECT nick, contrasena FROM usuarios WHERE nick ='" + usuario + "' AND contrasena='" + contrasena
+				+ "'";
+		st.executeQuery(q);
+
+		if (st.getResultSet() != null) {
+
+			return true;
+		} else {
+
+			return false;
+		}
+	}
+
+	// elimina un usuario de la bbdd segun el dni (Para el 3er Sprint)
+	// public void bajaUsuario(Usuario u) {
+	// Statement st = null;
+	// try {
+	// ConexionDB con = new ConexionDB();
+	// st = con.getConnection().createStatement();
+	// String q = "DELETE FROM `lac`.`usuarios` WHERE dni ='" + u.getDni() +
+	// "';";
+	// Pantalla.write(q);
+	// st.executeQuery(q);
+	// con.getConnection().close();
+	// } catch (SQLException ex) {
+	// Logger.getLogger(BackOfficeDAO.class.getName()).log(Level.SEVERE, null,
+	// ex);
+	// }
+	//
+	// }
+
+	// añade una nueva camiseta a la bbdd
+	public void altaCamiseta(Camiseta c) {
+		Statement st = null;
+		try {
+			ConexionDB con = new ConexionDB();
+			st = con.getConnection().createStatement();
+			String q = "INSERT INTO `lac`.`camisetas` VALUES ('" + c.getId() + "','" + c.getColor() + "','"
+					+ c.getPrecio() + "','" + c.getGenero() + "','" + c.getTalla() + "','" + c.getCategoria() + "','"
+					+ c.getStock() + "','" + c.getUrl() + "','" + c.getDescripcion() + "')";
+			Pantalla.write(q);
+			st.executeQuery(q);
+			con.getConnection().close();
+		} catch (SQLException ex) {
+			Logger.getLogger(BackOfficeDAO.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	// elimina una camiseta de la bbdd segun el id
+	// para el 3er sprint
+	// public void bajaCamiseta(Camiseta c) {
+	// Statement st = null;
+	// try {
+	// ConexionDB con = new ConexionDB();
+	// st = con.getConnection().createStatement();
+	// String q = "DELETE FROM `lac`.`camisetas` WHERE id ='" + c.getId() +
+	// "';";
+	// Pantalla.write(q);
+	// st.executeQuery(q);
+	// con.getConnection().close();
+	// } catch (SQLException ex) {
+	// Logger.getLogger(BackOfficeDAO.class.getName()).log(Level.SEVERE, null,
+	// ex);
+	// }
+	//
+	// }
 
 }
